a59394d867ba8625d8986232a93bfe3273600fed
20047223 <20047223@myrp.edu.sg>
2021-07-14 13:35:11 +0800

    update

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -81,6 +81,7 @@ public class ResourceCentreTest {
 	@Test
 	public void testRetrieveAllChromebook() {
 		assertNotNull("Check if there is a valid Camcorder arraylist to add to", camcorderList);
+		
 		//fail("Not yet implemented");
 		// write your code here
 	}
